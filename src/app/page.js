67555c92e4d1fb539be1ee69beeb1ba2976e5e53
Berkay Bideci <berkaybideci@gmail.com>
2023-06-08 22:33:44 +0300

    Completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,95 @@
-import Image from 'next/image'
+'use client'
+import { useForm } from "react-hook-form";
+import { useState } from "react"
 
-export default function Home() {
+export default function ReactForm() {
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const onSubmit = data => setSubmittedData(data);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container mx-auto h-screen flex flex-col justify-center items-center gap-5">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col px-5 w-full lg:w-3/4 xl:w-1/3">
+
+        <div className="my-2">
+          <input {...register("firstName", { required: true, pattern: /^[a-zA-Z]+(?:-[a-zA-Z]+)*$/ })}
+            placeholder="First Name"
+            className="bg-emerald-100 font-bold border-2 rounded-lg px-2 w-full"
+            aria-invalid={errors.firstName ? "true" : "false"} />
+          {errors.firstName?.type === 'required' && <p role="alert" className="text-end text-red-600 italic font-bold">First name is required</p>}
+          {errors.firstName?.type === 'pattern' && <p role="alert" className="text-end text-red-600 italic font-bold">First name is invalid</p>}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="my-2">
+          <input {...register("lastName", { required: true, pattern: /^[a-zA-Z]+(?:-[a-zA-Z]+)*$/ })}
+            placeholder="Last Name"
+            className="bg-emerald-100 font-bold border-2 rounded-lg px-2 w-full"
+            aria-invalid={errors.lastName ? "true" : "false"} />
+          {errors.lastName?.type === 'required' && <p role="alert" className="text-end text-red-600 italic font-bold">Last name is required</p>}
+          {errors.lastName?.type === 'pattern' && <p role="alert" className="text-end text-red-600 italic font-bold">Last name is invalid</p>}
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="my-2">
+          <input {...register("age", { required: true })}
+            type="number"
+            placeholder="Age"
+            className="bg-emerald-100 font-bold border-2 rounded-lg px-2 w-full"
+            aria-invalid={errors.age ? "true" : "false"} />
+          {errors.age?.type === 'required' && <p role="alert" className="text-end text-red-600 italic font-bold">Age is required</p>}
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="my-2">
+          <input {...register("email", { required: true, pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ })}
+            placeholder="Email"
+            className="bg-emerald-100 font-bold border-2 rounded-lg px-2 w-full"
+            aria-invalid={errors.email ? "true" : "false"} />
+          {errors.email?.type === 'required' && <p role="alert" className="text-end text-red-600 italic font-bold">Email is required</p>}
+          {errors.email?.type === 'pattern' && <p role="alert" className="text-end text-red-600 italic font-bold">Email is invalid</p>}
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <div className="my-2">
+          <textarea {...register("address", { required: true })}
+            placeholder="Address"
+            className="bg-emerald-100 font-bold border-2 rounded-lg px-2 w-full min-h-[2em]"
+            aria-invalid={errors.address ? "true" : "false"} />
+          {errors.address?.type === 'required' && <p role="alert" className="text-end text-red-600 italic font-bold">Address is required</p>}
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="my-2">
+          <input {...register("phone", {
+            required: true, pattern: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+          })}
+            type="number"
+            placeholder="Phone"
+            className="bg-emerald-100 font-bold border-2 rounded-lg px-2 w-full"
+            aria-invalid={errors.phone ? "true" : "false"} />
+          {errors.phone?.type === 'required' && <p role="alert" className="text-end text-red-600 italic font-bold">Phone is required</p>}
+          {errors.phone?.type === 'pattern' && <p role="alert" className="text-end text-red-600 italic font-bold">Phone is invalid</p>}
+        </div>
+
+        <div className="my-2 flex flex-col">
+          <label className="text-center text-emerald-100 font-bold" htmlFor="checkbox">I accept the <a href="#" className="text-emerald-500 italic">Terms & Conditions</a></label>
+          <input {...register("checkbox", { required: true })} type="checkbox" aria-invalid={errors.phone ? "true" : "false"} name="checkbox" className="accent-emerald-600" />
+          {errors.checkbox?.type === 'required' && <p role="alert" className="text-center text-red-600 italic font-bold">You must accept the Terms & Conditions to proceed.</p>}
+        </div>
+
+        <input type="submit" className="bg-emerald-700 h-10 text-emerald-100 font-bold rounded-lg" />
+      </form>
+      <section className="flex flex-col break-all px-5 w-full lg:w-3/4 xl:w-1/3">
+        {submittedData && (
+          <div className="text-start text-emerald-100 font-bold border-2 rounded-lg border-emerald-100 border-dashed px-5 py-2">
+            <h3 className="text-center text-2xl my-2">Submitted Data</h3>
+            <p><span className="text-emerald-500">First Name:</span> {submittedData.firstName}</p>
+            <p><span className="text-emerald-500">Last Name:</span> {submittedData.lastName}</p>
+            <p><span className="text-emerald-500">Age:</span> {submittedData.age}</p>
+            <p><span className="text-emerald-500">Email:</span> {submittedData.email}</p>
+            <p><span className="text-emerald-500">Address:</span> {submittedData.address}</p>
+            <p><span className="text-emerald-500">Phone:</span> {submittedData.phone}</p>
+            <p><span className="text-emerald-500">Terms & Conditions:</span> {submittedData.checkbox && "Accepted"}</p>
+          </div>
+        )}
+      </section>
+    </div>
+  );
 }
